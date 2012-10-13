9fb6ba615462eb893e5c0ad1a440e4ffc682211f
Wojciech Topolski <wojciech.topolski@gmail.com>
2012-10-13 18:31:50 +0200

    Add simple gui for tests

--- src/main/java/pl/wtopolski/android/polishnotation/MainActivity.java
+++ src/main/java/pl/wtopolski/android/polishnotation/MainActivity.java
@@ -2,9 +2,13 @@ package pl.wtopolski.android.polishnotation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private EditText edit;
+    private TextView test;
 
     static {
         System.loadLibrary("polish-notation");
@@ -15,14 +19,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        String request = "-1.1+3/2+5+6.5";
+        edit = (EditText) findViewById(R.id.edit);
+        edit.setText("1*0.5/3/8-4+3*10-5");
+
+        test = (TextView) findViewById(R.id.test);
+        test.setTextSize(22);
+        test.setPadding(4, 4, 4, 4);
+        test.setTextScaleX(0.9f);
+    }
+
+    public void action(View view) {
+        String request = edit.getText().toString();
         String response = JniHelper.convertToNotation(request);
         double result = JniHelper.countValueFromNotation(response);
 
-        TextView tv = (TextView) findViewById(R.id.test);
-        tv.setTextSize(22);
-        tv.setPadding(4, 4, 4, 4);
-        tv.setTextScaleX(0.9f);
-        tv.setText("request: " + request + "\nresponse: " + response + "\nresult: " + result);
+        test.setText("request: " + request + "\nresponse: " + response + "\nresult: " + result);
     }
 }
